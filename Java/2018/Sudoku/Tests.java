837914f7badd6e2583212d4e6b8e8f74111484cc
JoshCoop1089 <41652109+JoshCoop1089@users.noreply.github.com>
2018-11-27 19:56:51 -0500

    Board Start Function Round 1

--- Java/2018/Sudoku/Tests.java
+++ Java/2018/Sudoku/Tests.java
@@ -3,43 +3,48 @@
  * @Date:   2018-11-01T12:01:08-04:00
  * @Email:  joshcoop1089@gmail.com
  * @Last modified by:   Josh Cooper
- * @Last modified time: 2018-11-18T11:21:44-05:00
+ * @Last modified time: 2018-11-27T19:42:30-05:00
  */
 import java.util.*;
 public class Tests {
-  public static void main(String[] args) {
-    // int size = Board.chooseSize();
-    // if (size == 9) {}
-    // char difficulty = Board.chooseDifficulty();
-    // Board test = new Board(9,'e');
-    // test.currentState();
-    // test.boardPrint();
-    Board small = new Board(4,'e');
-    small.boardPrint();
-    small.currentBoardState();
-    small.boardPrint();
-    int row = 1;
-    int column = 3;
-    int number = 2;
-    int box = Board.boxChoose(row, column, small.getNum());
-
+  public static void replaceTester(int row, int column, int number, int box, Board current) {
     System.out.println("\nTesting Number placement/replacing");
     System.out.println("\nRow " + row + " was: \t" +
-                  Arrays.toString(small.getBoardState("Row " + row)));
+                  Arrays.toString(current.getBoardState("Row " + row)));
     System.out.println("Column " + column + " was: \t" +
-                  Arrays.toString(small.getBoardState("Column " + column)));
+                  Arrays.toString(current.getBoardState("Column " + column)));
     System.out.println("Box " + box + " was: \t" +
-                  Arrays.toString(small.getBoardState("Box " + box)));
+                  Arrays.toString(current.getBoardState("Box " + box)));
     System.out.println("\nPutting " + number + " at Row " + row + " Column " + column);
 
-    small.placeNumber(row,column,number);
+    current.placeNumber(row,column,number);
 
     System.out.println("\nRow " + row + " is now: \t" +
-                  Arrays.toString(small.getBoardState("Row " + row)));
+                  Arrays.toString(current.getBoardState("Row " + row)));
     System.out.println("Column " + column + " is now:" +
-                  Arrays.toString(small.getBoardState("Column " + column)));
+                  Arrays.toString(current.getBoardState("Column " + column)));
     System.out.println("Box " + box + " is now: \t" +
-                  Arrays.toString(small.getBoardState("Box " + box))+ "\n");
-    small.boardPrint();
+                  Arrays.toString(current.getBoardState("Box " + box))+ "\n");
+  }
+  public static void main(String[] args) {
+
+    Board small = Board.boardStart();
+    if (small != null) {
+      small.startBoardState();
+      System.out.println(small.getBoardFileName());
+      small.boardPrint();
+      for (int i = 1; i < 4; i++) {
+        int row = i;
+        int column = i;
+        int number = 2*i;
+        int box = Board.boxChoose(row, column, small.getNum());
+        replaceTester(row,column,number,box,small);
+        small.boardPrint();
+      }
+
+      System.out.println(small.getGameHistory(1));
+      System.out.println(small.getGameHistory(2));
+      System.out.println(small.getGameHistory(3));
+    }
   }
 }
--- Java/2018/Sudoku/board.java
+++ Java/2018/Sudoku/board.java
@@ -3,7 +3,10 @@
  * @Date:   2018-10-24T08:57:47-04:00
  * @Filename: board.java
  * @Last modified by:   Josh Cooper
- * @Last modified time: 2018-11-19T14:09:39-05:00
+ * @Last modified time: 2018-11-27T19:41:45-05:00
+ * Java Changes relative to Python v3b
+ *    Updated to OOP for practice manipulating personal class object
+ *    Switched history recorder from quadratic to linear
  */
 
 
@@ -12,110 +15,128 @@ public class Board {
 
   //Attributes
   private int num;  //will be required for calculating certain properties for box choice
-  private int gameCount; //Used to access specific game boards for UNdo function
-  private char difficulty = '';  // for choosing E/M/H versions of 9x9 boards
+  private int gameCount = 1; //Used to access specific game moves for Undo function
+  private char difficulty;  // for choosing E/M/H versions of 9x9 boards
+  private static Scanner input = new Scanner(System.in);
+  private String boardFileName; // for accessing the specific file with required boards
 
-  //Used for while loops for proper row/column checks
-  private boolean rowChosen;
-  private boolean columnChosen;
-  private boolean numberChosen;
+    //Used in Sudoku.java while loops for proper row/column checks
+  private boolean isRowChosen = false;
+  private boolean isColumnChosen = false;
+  private boolean isNumberChosen = false;
 
-  //Setting up the board arrays for restarting game and managing current board
+    //Setting up the board arrays for restarting game and managing current board
   private int[][] startBoard;  // should be filled in by FileIO from the premade text files eventually
   private int[][] currentBoard;  // will hold the currentBoard version of the board
-  private HashMap<String, int[]> boardState = new HashMap<String, int[]>();
-  private HashMap<Integer, int[][]> gameHistory = new HashMap<Integer, int[][]>();
+  private HashMap<String, int[]> boardState = new HashMap<String, int[]>(); //Unique checks rely on keys stored here
+  private HashMap<Integer, String> gameHistory = new HashMap<Integer, String>(); //Undo function relies on Strings stored here
 
 
   //Constructor
-  public Board(int size, char difficulty) { //Used for 9x9 with difficulty chosen
+  public Board(int size, char difficulty) {
     this.num = (int) Math.pow(size, 0.5);
     this.startBoard = new int[size][size];
     this.currentBoard = new int[size][size];
-    if (difficulty != '') {
-      this.difficulty = difficulty;
+    String diffWord = "";
+    if (difficulty == 'e') {
+      diffWord = " Easy.txt";
+    } else if (difficulty == 'm') {
+      diffWord = " Medium.txt";
+    } else if (difficulty == 'h') {
+      diffWord = " Hard.txt";
+    } else if (difficulty == '_') {
+      diffWord = ".txt";
     }
-    this.rowChosen = false;
-    this.columnChosen = false;
-    this.numberChosen = false;
+    this.boardFileName = size + "x" + size + diffWord;
   }
 
   //Getters
+  public int[][] getStartBoard() {
+    return this.startBoard;
+  }
   public int[] getBoardState(String key) {
     return this.boardState.get(key);
   }
-  public int[][] getGameHistory(int num) {
+  public int getCurrentBoardNumber(int row, int column) {
+    return this.currentBoard[row-1][column-1];
+  }
+  public int getGameCount() {
+    return this.gameCount;
+  }
+  public String getGameHistory(int num) {
     return this.gameHistory.get(num);
   }
+  public boolean getIsRowChosen() {
+    return this.isRowChosen;
+  }
+  public boolean getIsColumnChosen() {
+    return this.isColumnChosen;
+  }
+  public boolean getIsNumberChosen() {
+    return this.isNumberChosen;
+  }
+
   public int getNum() {
     return this.num;
   }
-  public boolean getrowChosen() {
-    return this.rowChosen;
+  public String getBoardFileName() {
+    return this.boardFileName;
   }
-  public boolean getColumnChosen() {
-    return this.columnChosen;
-  }
-  public boolean getNumberChosen() {
-    return this.numberChosen;
-  }
+
 
   //Setters
+  public void setIsRowChosen(boolean rowChosen) {
+    this.isRowChosen = rowChosen;
+  }
+  public void setIsColumnChosen(boolean columnChosen) {
+    this.isColumnChosen = columnChosen;
+  }
+  public void setIsNumberChosen(boolean numberChosen) {
+    this.isNumberChosen = numberChosen;
+  }
+  public void setCurrentBoardNumber(int row, int column, int number) {
+    this.currentBoard[row-1][column-1] = number;
+  }
 
-    //This should modify specific keys to update the state when you plug in new numbers
+  //This should modify specific keys to update the state when you plug in new numbers
   public void setBoardState(String key, int[] nums) {
     this.boardState.put(key,nums);
   }
-  public void setRowChosen(boolean rowChosen) {
-    this.rowChosen = rowChosen;
+    //This should store a move as a string and allow us to refer to later via undo function
+  public void setGameHistory(String lastMove) {
+    this.gameHistory.put(this.getGameCount(),lastMove);
   }
-  public void setColumnChosen(boolean columnChosen) {
-    this.columnChosen = columnChosen;
-  }
-  public void setNumberChosen(boolean numberChosen) {
-    this.numberChosen = numberChosen;
+  public void setBoardFileName(String boardFileName) {
+    this.boardFileName = boardFileName;
   }
 
-    //This should take the entire 2dInt array and store it to refer to later via undo function
-  public void setGameHistory(int gameCount, int[][] current) {
-    this.gameHistory.put(gameCount,current);
-  }
 
 
   //Methods To Finish
-  // public static int chooseSize() {
-  //   int size = 0;
-  //   try {
-  //     System.out.print("Please pick which type of board you want to play."+
-  //                       "\n Input 4 for a 4x4 game, or 9 for a 9x9 game."+
-  //                       "\n Input 99 to quit. ===> ");
-  //   catch (Exception e) {
-  //
-  //   }
-  //   return size;
-  // }
-  // public static char chooseDifficulty() {
-  //   char difficulty = '';
-  //   return difficulty;
-  // }
-  public static void chooseRandomBoard () {}
-  public int[][] boardStart() {
-    //load board from file, populate this.startBoard with proper digits,
-    // populate this.boardState with first set of row,column,box values
-    return this.startBoard;
-  }
-  public static void gameState () {}
-  public void chooseNumber(int number) {
 
-  }
-  public void chooseRow(int row) {}
-  public void chooseColumn(int column) {}
+  public static void chooseRandomBoard () {}
   public void undoMove() {
     System.out.println("The board will now be:\n");
-    this.current = this.gameHistory.get(this.gameCount)
+    this.gameCount--;
+    // this.currentBoard = this.gameHistory.get(this.gameCount);
     this.boardPrint();
   }
-
+  public int getLastMoveOriginalNum(String lastMove) {
+    int origNum = Integer.parseInt(lastMove.substring(9,10));
+    return origNum;
+  }
+  public int getLastMoveOriginalRow(String lastMove) {
+    int origRow = Integer.parseInt(lastMove.substring(18,19));
+    return origRow;
+  }
+  public int getLastMoveOriginalColumn(String lastMove) {
+    int origColumn = Integer.parseInt(lastMove.substring(27,28));
+    return origColumn;
+  }
+  public int getLastMoveNewNumber(String lastMove) {
+    int newNumber = Integer.parseInt(lastMove.substring(34,35));
+    return newNumber;
+  }
 
   //Complete Methods
   public void boardPrint() {
@@ -128,7 +149,7 @@ public class Board {
 
     //Create Column Numbers for Header
     Map<Integer, String> columnMap = new HashMap<>();
-    while (columnCount < this.num) {
+    while (columnCount < this.getNum()) {
       String columns = "";
       for (int i = columnHold; i < columnHold + num; i++) {
         columns += i + "  ";
@@ -151,9 +172,9 @@ public class Board {
     for (int i = 0; i < dashCount; i++) {
       dashes += "-";
     }
-    System.out.println(space + " Column #");
-    System.out.println("||Row  " + columnList);
-    System.out.println("\\/# " + dashes);
+    System.out.println(space + "  Column #");
+    System.out.println("||Row   " + columnList);
+    System.out.println("\\/ # " + dashes);
 
     //Internal Board Parts
     Map<Integer, String> boardChunk = new HashMap<>();
@@ -171,16 +192,90 @@ public class Board {
       for (int i = numHold; i < num + numHold; i++) {
         rowOut += " | " + boardChunk.get(i);
       }
-      System.out.println("  " + Integer.toString(rowShow) + rowOut + " |");
+      System.out.println("   " + Integer.toString(rowShow) + rowOut + " |");
       numHold += num;
       rowShow++;
       rowOut = "";
       if (rowShow%num == 1) {
-        System.out.println("    " + dashes);
+        System.out.println("     " + dashes);
       }
     }
   }
-  public void currentBoardState() {
+  public static String chooseBoardSizeAndDifficulty() {
+    Set<Integer> sizeSet = new HashSet<Integer>();
+    sizeSet.add(4);
+    sizeSet.add(9);
+    sizeSet.add(99);
+    int size = 0;
+    // try {
+      System.out.print("Please pick which type of board you want to play."+
+                        "\n\tInput 4 for a 4x4 game, or 9 for a 9x9 game."+
+                        "\n\tInput 99 to quit. ===> ");
+      size = input.nextInt();
+      if (!sizeSet.contains(size)) {
+        //spit out an exception;
+      }
+      if (size == 99) {
+        return "";
+      }
+      if (size == 4) {
+        return "4";
+      } else {
+        String diff = chooseDifficulty();
+        if (diff.equals("-")) {
+          return "";
+        } else {
+          return "9" + diff;
+        }
+      }
+    // }
+    // catch (Exception e) {
+    //
+    // }
+  }
+  public static String chooseDifficulty() {
+    Set<String> difficultySet = new HashSet<String>();
+    difficultySet.add("e");
+    difficultySet.add("m");
+    difficultySet.add("h");
+    difficultySet.add("q");
+    String difficulty = "";
+    // try {
+      System.out.print("What level difficulty would you like to try?"+
+                              "\n\tInput e for easy, m for medium, or  h for hard."+
+                              "\n\tInput q to quit. ===> ");
+      difficulty = input.next();
+      if (!difficultySet.contains(difficulty)) {
+        //spit out error
+      }
+      if (difficulty.equals("q")) {
+        return "-";
+      } else {
+        return difficulty;
+      }
+    //
+    // } catch (Exception e) {
+    //
+    // }
+  }
+  public static Board boardStart() {
+    //load board from file, populate this.startBoard with proper digits,
+    // populate this.boardState with first set of row,column,box values
+    String boardSizeAndDifficulty = chooseBoardSizeAndDifficulty();
+    if (boardSizeAndDifficulty.length() == 0) {
+      System.out.println("Guess we aren't playing after all!");
+      return null;
+    }
+    char difficulty = '_';
+    int size = Integer.parseInt(boardSizeAndDifficulty.substring(0,1));
+    if (boardSizeAndDifficulty.length() > 1) {
+      difficulty = boardSizeAndDifficulty.charAt(1);
+    }
+    Board startBoard = new Board(size, difficulty);
+    return startBoard;
+  }
+  public void startBoardState() {
+
     this.currentBoard[1][1] = 2;
     this.currentBoard[0][2] = 4;
     this.currentBoard[3][3] = 6;
@@ -189,6 +284,7 @@ public class Board {
     this.currentBoard[0][3] = 4;
     this.currentBoard[2][3] = 6;
     this.currentBoard[1][3] = 8;
+
     //Internal helper for box aggregation
     HashMap<String, List<Integer>> boxes= new HashMap<>();
     List<Integer> boxHelper = new ArrayList<>();
@@ -231,7 +327,7 @@ public class Board {
   }
   public boolean rowUnique (int row, int number) {
       boolean rowUnique = true;
-      for (int i : this.boardState.get("Row " + row)) {
+      for (int i : this.getBoardState("Row " + row)) {
           if (i == number) {
               rowUnique = false;
           }
@@ -239,7 +335,7 @@ public class Board {
   }
   public boolean boxUnique (int box, int number) {
       boolean boxUnique = true;
-      for (int i : this.boardState.get("Box "+ box)) {
+      for (int i : this.getBoardState("Box "+ box)) {
           if (i == number) {
               return false;
           }
@@ -247,11 +343,48 @@ public class Board {
   }
   public boolean columnUnique (int column, int number) {
       boolean columnUnique = true;
-      for (int i : this.boardState.get("Column " + column)) {
+      for (int i : this.getBoardState("Column " + column)) {
           if (i == number) {
               return false;
           }
       } return columnUnique;
+  }
+  public void chooseRow(int row) {
+    if (row > 0 && row <= this.startBoard.length) {
+      this.setIsRowChosen(true);
+    } else if (row == 88) {
+      //Undo function activates
+    } else if (row == 99) {
+      //quit function activates
+    } else {
+      System.out.println("Please choose a row between 1 and " + this.startBoard.length
+       + ", 88 to undo a move, or 99 to quit the current game.");
+    }
+
+  }
+  public void chooseColumn(int column) {
+    if (column > 0 && column <= this.startBoard.length) {
+      this.setIsColumnChosen(true);
+    } else if (column == 88) {
+      //Undo function activates
+    } else if (column == 99) {
+      //quit function activates
+    } else {
+      System.out.println("Please choose a column between 1 and " + this.startBoard.length
+       + ", 88 to undo a move, or 99 to quit the current game.");
+    }
+  }
+  public void chooseNumber(int number, int row, int column) {
+    if (number > 0 && number <= this.startBoard.length) {
+      this.setIsNumberChosen(true);
+    } else if (number == 88) {
+      //Undo function activates
+    } else if (number == 99) {
+      //quit function activates
+    } else {
+      System.out.println("Please choose a number between 1 and " + this.startBoard.length
+       + ", 88 to undo a move, or 99 to quit the current game.");
+    }
   }
   public int[] replaceNumber(int[] arrayHelper, int numHold, int number) {
     for (int i = 0; i < arrayHelper.length; i++) {
@@ -265,7 +398,9 @@ public class Board {
     //this version of placeNumber assumes valid placement
   public void placeNumber(int row, int column, int number) {
     //Holding the number to check inside the replaceNumber function
-    int numHold = this.currentBoard[row-1][column-1];
+    int numHold = this.getCurrentBoardNumber(row,num);
+    String lastMove = writeLastMove(row,column, number);
+    this.setGameHistory(lastMove);
 
     //Put the number in the Row Key
     int[] arrayHelper = this.getBoardState("Row " + row);
@@ -281,9 +416,15 @@ public class Board {
     arrayHelper = this.getBoardState("Box " + boxChoose(row, column, this.num));
     arrayHelper = this.replaceNumber(arrayHelper, numHold, number);
     this.setBoardState("Box " + boxChoose(row,column,this.num), arrayHelper);
-    this.currentBoard[row-1][column-1] = number;
+
+
+    this.setCurrentBoardNumber(row, column, number);
+    this.gameCount++;
 
   }
-
-
+  public String writeLastMove(int row, int column, int number) {
+    int originalNum = this.getCurrentBoardNumber(row,column);
+    String lastMove = "Replaced " + originalNum + " at Row " + row + " Column " + column + " with " + number;
+    return lastMove;
+  }
 }
